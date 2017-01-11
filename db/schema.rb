a7284c85eed9818a664cceff733645c5511bed6a
qdlin <qdlin1314@gmail.com>
2017-01-11 13:52:49 +0800

    implement part4

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,12 @@ ActiveRecord::Schema.define(version: 20170110031307) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
